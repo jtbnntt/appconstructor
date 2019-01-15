f52db966740d97aabd32be1c7b22d4d735b1a1f7
jtbnntt <10692730+jtbnntt@users.noreply.github.com>
2019-01-14 18:10:51 -0800

    Add metadata to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('README.md') as file:
+    long_description = file.read()
 
 setup(
     name='appconstructor',
-    packages=['appconstructor'])
+    version='0.0.0',
+    description='Tool to create apps with injected resources',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages())
